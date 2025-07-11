2938916a6a6127cec13e451622e0ed48e1e6d4d3
chy031202 <chy031202@gmail.com>
2025-07-11 16:10:19 +0900

    [Design] 폰트 CSS 수정

--- src/components/modals/EditDeleteModal.tsx
+++ src/components/modals/EditDeleteModal.tsx
@@ -27,11 +27,11 @@ const EditDeleteBottomSheet = ({ isOpen, onRequestClose }: ModalProps) => {
                 <Sheet.Content>
                     <div className="flex flex-col justify-center items-center p-10 w-full text-center mb-5">
                         <h3
-                            style={{ fontFamily: 'Ydestreet', fontWeight: '700' }}
+                            style={{ fontFamily: 'Ydestreet', fontWeight: '700', fontSize:'14px' }}
                             className="text-lg font-semibold mb-6 w-full whitespace-nowrap text-center text-black">
                             축하글을 수정하거나 삭제하시겠어요?</h3>
 
-                        <div className="flex flex-row justify-center gap-[10px]">
+                        <div className="flex flex-row justify-center gap-[12px]">
                             <CustomButton
                                 className="px-[15px] py-[15px] w-fit whitespace-nowrap text-center"
                             >축하글 수정하기</CustomButton>
